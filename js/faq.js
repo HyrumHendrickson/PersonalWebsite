e434c8e83e2ea40686c453f55c984bf0171aecdd
HyrumHendrickson <hendrickson.hyrum@gmail.com>
2024-10-24 09:39:06 -0600

    restarted

--- js/faq.js
+++
@@ -1,8 +0,0 @@
-// Initialize the map
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-
-// Add a tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(mymap);
-
